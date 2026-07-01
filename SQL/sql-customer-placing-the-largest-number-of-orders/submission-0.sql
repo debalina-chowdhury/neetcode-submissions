3e7a96a32f0dfd730fe0c62907a403727983d4a4
Debalina Chowdhury <40907834+debalina-chowdhury@users.noreply.github.com>
2026-06-30 19:55:40 -0700

    Add: sql-customer-placing-the-largest-number-of-orders - submission-0

---
+++ SQL/sql-customer-placing-the-largest-number-of-orders/submission-0.sql
@@ -0,0 +1,6 @@
+-- Write your query below
+select customer_number from
+(select customer_number, count(order_number)
+from orders
+group by customer_number
+order by count(order_number) desc limit 1)
