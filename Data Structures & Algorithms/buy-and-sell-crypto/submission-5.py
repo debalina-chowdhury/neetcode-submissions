697462fd531d3572ab0d98c1fc094c2e6afb2923
Debalina Chowdhury <40907834+debalina-chowdhury@users.noreply.github.com>
2026-08-09 17:04:11 -0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        profit = 0
+        for p in prices:
+            min_price = min(min_price, p)      # cheapest buy day up to here
+            profit = max(profit, p - min_price)  # best sell if we sell today
+        return profit
+
+        #O(n), O(n)
